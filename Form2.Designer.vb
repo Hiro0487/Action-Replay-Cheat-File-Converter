e01e55513fcbf55c061c68b864b4921fa40b828f
Hiro0487 <Brady_96@yahoo.com>
2024-01-08 13:37:22 -0500

    Started adding ability to converter melonDS mch filr to DeSmuME dct file

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Frm2_Preview
+Partial Class Frm2_melonDS_to_DeSmuMe
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,73 +22,100 @@ Partial Class Frm2_Preview
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
+        components = New ComponentModel.Container()
         SaveFileDialog1 = New SaveFileDialog()
-        Frm2_Save_Btn = New Button()
+        ToolTip1 = New ToolTip(components)
+        TextBox1 = New TextBox()
+        Button1 = New Button()
         Button2 = New Button()
+        TextBox2 = New TextBox()
+        Frm2_Close_Btn = New Button()
         SuspendLayout()
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(12, 12)
-        TextBox1.Multiline = True
+        TextBox1.Location = New Point(161, 70)
         TextBox1.Name = "TextBox1"
-        TextBox1.PlaceholderText = "Source Fille to Convert Preview"
-        TextBox1.ReadOnly = True
-        TextBox1.Size = New Size(381, 384)
-        TextBox1.TabIndex = 0
+        TextBox1.Size = New Size(143, 23)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "Game Title"
+        ToolTip1.SetToolTip(TextBox1, "In melonDS can be found by going to System in menu bard and clicking on ROM Info")
         ' 
-        ' TextBox2
+        ' Button1
         ' 
-        TextBox2.Location = New Point(399, 12)
-        TextBox2.Multiline = True
-        TextBox2.Name = "TextBox2"
-        TextBox2.PlaceholderText = "Target Converted Preview"
-        TextBox2.ReadOnly = True
-        TextBox2.Size = New Size(398, 384)
-        TextBox2.TabIndex = 1
-        ' 
-        ' Frm2_Save_Btn
-        ' 
-        Frm2_Save_Btn.Location = New Point(12, 402)
-        Frm2_Save_Btn.Name = "Frm2_Save_Btn"
-        Frm2_Save_Btn.Size = New Size(381, 46)
-        Frm2_Save_Btn.TabIndex = 2
-        Frm2_Save_Btn.Text = "Save"
-        Frm2_Save_Btn.UseVisualStyleBackColor = True
+        Button1.BackColor = Color.Transparent
+        Button1.BackgroundImage = My.Resources.Resources.Provide_Game_Title
+        Button1.BackgroundImageLayout = ImageLayout.Stretch
+        Button1.CausesValidation = False
+        Button1.Enabled = False
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.ForeColor = Color.Transparent
+        Button1.Image = My.Resources.Resources.Provide_Game_Title
+        Button1.Location = New Point(10, 8)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(145, 85)
+        Button1.TabIndex = 2
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(399, 402)
+        Button2.BackColor = Color.Transparent
+        Button2.BackgroundImage = My.Resources.Resources.Provide_Game_Title
+        Button2.BackgroundImageLayout = ImageLayout.Stretch
+        Button2.CausesValidation = False
+        Button2.Enabled = False
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.ForeColor = Color.Transparent
+        Button2.Image = My.Resources.Resources.Provide_Game_Title
+        Button2.Location = New Point(12, 99)
         Button2.Name = "Button2"
-        Button2.Size = New Size(398, 46)
-        Button2.TabIndex = 3
-        Button2.Text = "Cancel"
-        Button2.UseVisualStyleBackColor = True
+        Button2.Size = New Size(145, 85)
+        Button2.TabIndex = 4
+        Button2.UseVisualStyleBackColor = False
         ' 
-        ' Frm2_Preview
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(163, 161)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(143, 23)
+        TextBox2.TabIndex = 3
+        TextBox2.Text = "Game Code"
+        ' 
+        ' Frm2_Close_Btn
+        ' 
+        Frm2_Close_Btn.ForeColor = Color.Black
+        Frm2_Close_Btn.Location = New Point(10, 194)
+        Frm2_Close_Btn.Name = "Frm2_Close_Btn"
+        Frm2_Close_Btn.Size = New Size(307, 23)
+        Frm2_Close_Btn.TabIndex = 5
+        Frm2_Close_Btn.Text = "Close"
+        Frm2_Close_Btn.UseVisualStyleBackColor = True
+        ' 
+        ' Frm2_melonDS_to_DeSmuMe
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImage = My.Resources.Resources.FutureLoginModified
         BackgroundImageLayout = ImageLayout.Stretch
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(329, 229)
         ControlBox = False
+        Controls.Add(Frm2_Close_Btn)
         Controls.Add(Button2)
-        Controls.Add(Frm2_Save_Btn)
         Controls.Add(TextBox2)
+        Controls.Add(Button1)
         Controls.Add(TextBox1)
         DoubleBuffered = True
-        Name = "Frm2_Preview"
+        ForeColor = Color.Transparent
+        Name = "Frm2_melonDS_to_DeSmuMe"
         Text = "Conversion Preview"
         ResumeLayout(False)
         PerformLayout()
     End Sub
-
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
-    Friend WithEvents Frm2_Save_Btn As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Frm2_Close_Btn As Button
 End Class
--- Form2.vb
+++ Form2.vb
@@ -1,13 +1,144 @@
-﻿Public Class Frm2_Preview
-    Private Sub Frm2_Preview_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
+
+Public Class Frm2_melonDS_to_DeSmuMe
+    Private Sub Frm2_melonDS_to_DeSmuMe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub Frm2_Save_Btn_Click(sender As Object, e As EventArgs) Handles Frm2_Save_Btn.Click
+    Private Sub Frm2_Close_Btn_Click(sender As Object, e As EventArgs) Handles Frm2_Close_Btn.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Function PromptUserForFile() As String
+        Dim openFileDialog As New OpenFileDialog With {
+            .Title = "Select Cheat File",
+            .Filter = "Cheat Files (*.MCH)|*.MCH",
+            .InitialDirectory = "F:\Emulator's\ROMS\DS\Pokemon Diamond (v05) (U)(Legacy)"'This will be dynamic when project is finished
+        }
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = openFileDialog.FileName
+            Return filePath ' Return the selected file path
+        Else
+            Return String.Empty ' Return an empty string if the user canceled
+        End If
+    End Function
+
+    Private Sub ProcessCheatFile()
+        Dim invalidHexCodes As List(Of String) = New List(Of String)
+
+        Dim filePath = PromptUserForFile()
+        Dim lines = File.ReadAllLines(filePath).Skip(4).ToArray()
+
+        Dim outputLines As List(Of String) = New List(Of String)
+        For Each line As String In lines
+            ProcessLine(line, outputLines, invalidHexCodes)
+        Next
+
+        Dim saveFilePath = PromptUserForSaveFile()
+        File.WriteAllLines(saveFilePath, outputLines.ToArray())
+        MsgBox("Cheat file converted successfully.")
+
+        DisplayInvalidCodesMessage(invalidHexCodes)
+    End Sub
+
+    Private Sub ProcessLine(line As String, outputLines As List(Of String), invalidHexCodes As List(Of String))
+        Select Case True
+            Case line.StartsWith(";") : ProcessCommentLine(line, outputLines)
+            Case line.StartsWith("AR 1") OrElse line.StartsWith("AR 0") : ProcessCodeLine(line, outputLines, invalidHexCodes)
+            Case Else : ProcessHexCodeLine(line, outputLines, invalidHexCodes)
+        End Select
+    End Sub
+
+    Private Sub ProcessCommentLine(line As String, outputLines As List(Of String))
+        Dim hexCodesAndDescription = line.Substring(1).Trim().Split({","c, ";"c}, StringSplitOptions.RemoveEmptyEntries)
+        Dim codeType = If(line.StartsWith(";AR 1 "), "CODE 1", "CAT") ' Determine code type
+        Dim description = hexCodesAndDescription.Last().Trim() ' Trim potential extra spaces
+        description = description & Environment.NewLine
+        ' Add description to code type line
+        outputLines.Add($"{codeType} {description}")
+    End Sub
+
+    Private Sub ProcessCodeLine(line As String, outputLines As List(Of String), invalidHexCodes As List(Of String))
+        ' ... (code for processing code lines, including category handling)
+        Dim description As String
+        Dim hexCodes As String()
+        ' Use a temporary variable to hold the tuple
+        Dim result = ExtractDescriptionAndHexCodes(line)
+        description = result.Description
+        hexCodes = result.HexCodes
+        Dim codeType = DetermineCodeType(line)
+        outputLines.Add($"{codeType} {description}")
+        ProcessHexCodes(hexCodes, outputLines, invalidHexCodes)
+        outputLines.Add(Environment.NewLine)
+    End Sub
+
+    Private Sub ProcessHexCodeLine(line As String, outputLines As List(Of String), invalidHexCodes As List(Of String))
+        ' ... (code for processing hex code lines)
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Close()
+    Private Function PromptUserForSaveFile() As String
+        Dim outputLines As List(Of String) = New List(Of String)
+        Dim saveFileDialog As New SaveFileDialog With {
+            .Title = "Save Converted Cheat File",
+            .Filter = "Cheat Files (*.DCT)|*.DCT",
+            .InitialDirectory = "F:\Emulator's\DS\DeSmuMe\Cheats"'This will be dynamic when project is finished
+            }
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Return saveFileDialog.FileName ' Return the selected file path
+        Else
+            Return String.Empty ' Return an empty string if the user canceled
+        End If
+    End Function
+
+    Private Sub DisplayInvalidCodesMessage(invalidHexCodes As List(Of String))
+        ' Display a message about invalid codes (optional)
+        If invalidHexCodes.Count > 0 Then
+            MsgBox("The following hex codes were invalid: " & Environment.NewLine & String.Join(Environment.NewLine, invalidHexCodes))
+        End If
+    End Sub
+
+    Private Function ExtractDescriptionAndHexCodes(line As String) As (Description As String, HexCodes As String())
+        Dim descriptionStartIndex = line.IndexOf(";")
+        If descriptionStartIndex >= 0 Then
+            Dim description = line.Substring(descriptionStartIndex + 1).Trim()
+            Dim hexCodesAndDescription = line.Substring(4, descriptionStartIndex - 4).Trim()
+            Dim hexCodes = hexCodesAndDescription.Split({","c}, StringSplitOptions.RemoveEmptyEntries).ToArray()
+            Return (description, hexCodes)
+        End If
+        Return ("", {}) ' Return empty values if description and hex codes are not found
+    End Function
+
+    Private Function DetermineCodeType(line As String) As String
+        Return If(line.StartsWith("AR 1 "), "CODE 1", "CODE 0")
+    End Function
+
+    Private Sub ProcessHexCodes(hexCodes As String(), outputLines As List(Of String), invalidHexCodes As List(Of String))
+        For Each hexCode As String In hexCodes
+            Try
+                If IsValidHexCode(hexCode) Then
+                    ProcessValidHexCode(hexCode, outputLines)
+                Else
+                    outputLines.Add(hexCode)
+                    invalidHexCodes.Add(hexCode)
+                End If
+            Catch ex As Exception
+                outputLines.Add("Error processing hex code: " & ex.Message)
+            End Try
+        Next
+    End Sub
+
+    Private Function IsValidHexCode(hexCode As String) As Boolean
+        Return hexCode.Replace(" ", "").All(Function(c) Char.IsDigit(c) OrElse Char.IsLetter(c))
+    End Function
+
+    Private Sub ProcessValidHexCode(hexCode As String, outputLines As List(Of String))
+        Dim substringLength = If(hexCode.Length > 18, 18, 8)
+        If hexCode.Length >= substringLength Then
+            outputLines.Add(hexCode.Substring(0, substringLength) + " " + hexCode.Substring(substringLength))
+        Else
+            ' Handle the case where the string is too short (e.g., output the original hexCode or log a warning)
+        End If
     End Sub
 End Class
